5d50fa6911051961e52d320e16868bb9603acb9e
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-23 21:35:45 +0300

    2304

---
+++ 1704/WebApplication1/Controllers/RestController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RestController : ControllerBase
+    {
+        //[HttpGet("[action]/{id}")]
+        //public IActionResult Index(int id)
+        //{
+        //    return id % 2 != 0 ? BadRequest() : Ok("Число чётное");
+        //}
+        // -----------------------------
+        //[HttpGet("{id}/[action]")]
+        //public IActionResult Index(int id)
+        //{
+        //     return id % 2 != 0 ? BadRequest() : Ok("Число чётное");
+        //}
+        //--------------------
+
+        // https://localhost:7100/api/rest/index?id=1
+        //[HttpGet("[action]")]
+        //public IActionResult Index(int id)
+        //{
+        //    return id % 2 != 0 ? BadRequest() : Ok("Число чётное");
+        //}
+        //--------------------
+        //https://localhost:7100/api/rest/check-number/2
+        //[HttpGet("check-number/{id}")]
+        //public IActionResult Index(int id)
+        //{
+        //    return id % 2 != 0 ? BadRequest() : Ok("Число чётное");
+        //}
+
+        //--------------------
+
+
+        //PUT https://localhost:7100/api/rest/check-number/1
+        //[HttpPut("check-number/{id}")]
+        //public IActionResult Index(int id)
+        //{
+        //    return id % 2 != 0 ? BadRequest() : Ok("Число чётное");
+        //}
+
+
+        //        3.
+        //На сервере есть файл сервисов.В нем глобально хранится массив.Прописать
+        //логику так, чтобы при get запросе отправлялся ответ клиенту с массивом внутри
+        //тела
+        //[HttpPut("check-number/{id}")]
+        //public IActionResult GetArray()
+        //{
+        //    return Ok(ArrayService.Array);
+
+        //}
+
+        //--------------------
+        //[HttpGet("check-number/{id}")]
+        //public string[] GetArray()
+        //{
+        //    return ArrayService.Array;
+        //}
+        //        5.
+        //Добавить 1 обработчик маршрута : get с url “/:id”. На сервере есть файл сервисов.В
+        //нем глобально хранится массив объектов.Прописать логику так, чтобы при get
+        //запросе отправлялся ответ клиенту с объектом, id которого совпадает с
+        //запрашиваемым id, статус ответа
+        [HttpGet("[action]/{index}")]
+        public IActionResult GetArray(int index)
+        {
+            try
+            {
+                return Ok(ArrayService.Array[index - 1]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return BadRequest();
+            }
+        }
+        [HttpGet("[action]")]
+        public List<string> GetArray()
+        {
+
+            return ArrayService.Array;
+
+
+        }
+
+        //        6.
+        //Для post запроса добавить добавление тела запроса в массив и возвращать
+        //обновленный массив, статус ответа
+        [HttpPost("[action]")]
+        public IActionResult AddItem(ArrayItemModel data)
+        {
+            if (data.Item == null) return BadRequest();
+            ArrayService.Array.Add(data.Item);
+            return Ok(GetArray());
+        }
+
+
+        //        7.
+        //Добавить 2 обработчика маршрута put, delete
+        //https://localhost:7100/api/rest/DeleteByIndex/2
+        [HttpDelete("[action]/{index}")]
+
+        public IActionResult DeleteByIndex(int index)
+        {
+            try
+            {
+                ArrayService.Array.RemoveAt(index);
+                return Ok(ArrayService.Array);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return BadRequest();
+            }
+        }
+
+
+        [HttpPut("[action]/{index}")]
+
+
+        //https://localhost:7100/api/rest/UpdateItem/1
+        public IActionResult UpdateItem([FromBody]ArrayItemModel data, int index)
+        {
+            if (data.Item == null) return BadRequest();
+
+            try
+            {
+
+                ArrayService.Array[index-1] = data.Item;
+                return Ok(GetArray());
+            }
+            catch(ArgumentOutOfRangeException)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
