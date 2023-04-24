d8668f7c43200e54ec2b15fdf94da61a2b6b45f4
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-24 13:58:28 +0300

    2404

---
+++ 1704/WebApplication1/Services/ArrayService.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Services
+{
+    public class ArrayService
+    {
+
+        public static List<string> Array = new List<string> { "hello", "world", "Heh", "Heh1", "Heh2", "Heh3", "Heh4", "Heh5" };
+    }
+}
