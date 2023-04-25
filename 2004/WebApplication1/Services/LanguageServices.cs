ef517dc0903bda9e408d3614c5868fdbd7593867
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-25 11:51:26 +0300

    2504

---
+++ 2004/WebApplication1/Services/LanguageServices.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Text;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Services
+{
+    public class LanguageServices
+    {
+        
+        public async Task<List<Language>> GetAll()
+        {
+
+            return await new LanguageRepositories().GetAll();
+
+        }
+
+        public async Task Create(Language lg)
+        {
+            var list = await new LanguageRepositories().GetAll();
+            var item = list.FirstOrDefault(x => x.Id.ToLower() == lg.Id.ToLower());
+            if(item != null) 
+            {
+                throw new Exception();
+            }
+            list.Add(lg);
+            await new LanguageRepositories().WriteNew(list);
+        }
+
+    }
+}
