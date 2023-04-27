8d8edc3e49b91b092bc2b0e17e1a4e52adb1f967
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-27 08:00:05 +0300

    2704

---
+++ 2604/OnlineShop/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Repositories;
+
+namespace OnlineShop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserRepository m_Service;
+        public UserController(UserRepository userRepository)
+        {
+            m_Service = userRepository;
+        }
+        //GET “/user”
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(m_Service.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
