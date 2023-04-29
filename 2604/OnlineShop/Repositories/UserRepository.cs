d10864f390fd6c5a3d00996bdc339e73c90775f4
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-29 15:23:08 +0300

    2904

--- 2604/OnlineShop/Repositories/UserRepository.cs
+++ 2604/OnlineShop/Repositories/UserRepository.cs
@@ -1,15 +1,18 @@
 ﻿using MySql.Data.MySqlClient;
 using OnlineShop.Models;
+using System.Text.Json;
 
 namespace OnlineShop.Repositories
 {
-    public class UserRepository
+    public class UserRepository : BaseRepository
     {
         private readonly string SQL_GET_ALL = "select Id,Name,Surname,Email,Password from Users;";
-        private readonly MySqlConnection m_Connection;
-        public UserRepository(MySqlConnection connection)
+
+        private readonly string SQL_insertItem = "insert into Users(NAME, SURNAME,Email,Password) values {0} ;";
+
+        public UserRepository(MySqlConnection connection) : base(connection)
         {
-            m_Connection = connection;
+
         }
 
         public List<User> GetAll()
@@ -44,5 +47,66 @@ namespace OnlineShop.Repositories
                 m_Connection.Close();
             }
         }
+
+        public User GetById(int id)
+        {
+
+            try
+            {
+                var User = GetAll();
+                return User.FirstOrDefault(x => x.Id == id);
+
+            }
+
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                m_Connection.Close();
+            }
+
+        }
+
+        //public void Test(MySqlConnection conn, string sqript, params object[] item)
+        //{
+        //    // ? -> @pr{i}
+        //    MySqlCommand command = new MySqlCommand(sqript, conn);
+        //    for (int i = 0; i < item.Length; i++)
+        //    {
+        //        command.Parameters.AddWithValue($"pr{i}", item[i]);
+        //    }
+        //    command.ExecuteNonQuery();
+        //}
+
+        public void WriteNew(User user)
+        {
+            try
+            {
+                if (user == null) throw new ArgumentNullException(nameof(user));
+                m_Connection.Open();
+                if (m_Connection == null) throw new Exception("connection is null");
+
+                MySqlCommand command = new MySqlCommand(string.Format(SQL_insertItem, "(@name0, @surname0,email0)"), m_Connection);
+                command.Parameters.AddWithValue("@name0", user.Name);
+                command.Parameters.AddWithValue("@surname0", user.Surname);
+                command.Parameters.AddWithValue("@email0", user.Email);
+                command.Parameters.AddWithValue("@password0", user.Password);
+                command.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
+
+            finally
+            {
+                m_Connection.Close();
+            }
+        }
+      
     }
 }
