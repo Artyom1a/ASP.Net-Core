08a23c6796e8f6d26937670c151d9bea28f925f9
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-29 15:22:18 +0300

    2804

--- 2604/OnlineShop/Controllers/UserController.cs
+++ 2604/OnlineShop/Controllers/UserController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using OnlineShop.Models;
 using OnlineShop.Repositories;
+using OnlineShop.Services;
+using System.Data.Common;
 
 namespace OnlineShop.Controllers
 {
@@ -8,10 +12,13 @@ namespace OnlineShop.Controllers
     public class UserController : ControllerBase
     {
         private readonly UserRepository m_Service;
-        public UserController(UserRepository userRepository)
+        private readonly MySqlConnection k_Connection;
+        public UserController(MySqlConnection connection, UserRepository userRepository)
         {
+            k_Connection = connection;
             m_Service = userRepository;
         }
+
         //GET “/user”
         [HttpGet]
         public IActionResult Get()
@@ -26,5 +33,40 @@ namespace OnlineShop.Controllers
             }
 
         }
+
+        [HttpGet("getbyid/{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                return Ok(m_Service.GetById(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        //        POST “/user” – с клиента приходит объект вида { "name": "Hanna", "surname":
+        //"Pleshko", "email": "hannapleshko@gmail.com", "pwd": "12345678" }. Добавить в БД
+        //объект в том случае, если совпадений с почтой в БД нет.Вернуть клиенту коду
+        //нового объекта, статус
+        [HttpPost("getbyid/{id}")]
+        public IActionResult Create(User user)
+        {
+            try
+            {
+                UserServices userServices = new UserServices(k_Connection);
+                userServices.Create(user);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
     }
 }
