9683c870d5c939c57da2047afd0f1560c5e4bf30
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-30 22:25:27 +0300

    3004

---
+++ 2604/OnlineShop/Services/UserServices.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Text.Json;
+using System.Text;
+using OnlineShop.Models;
+using OnlineShop.Repositories;
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace OnlineShop.Services
+{
+    public class UserServices
+    {
+      //  private readonly UserRepository m_service
+        private readonly UserRepository m_userrep;
+        //public UserServices(UserRepository rep)
+        //{
+        //    m_Service = rep ?? throw new ArgumentNullException(nameof(rep));
+        //}
+        public UserServices(UserRepository repository)
+        {
+            m_userrep = repository;
+        }
+
+        public void Create(User user)
+        {
+            var list = m_userrep.GetAll();
+            var item = list.FirstOrDefault(x => x.Email == user.Email);
+            if (item != null)
+            {
+                throw new Exception();
+            }
+            //list.Add(user);
+            m_userrep.WriteNew(user);
+        }
+    }
+}
