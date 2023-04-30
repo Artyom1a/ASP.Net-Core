5c322ab7026920ecfdca2f9067fb5954107cf0d7
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-04-30 22:25:09 +0300

    3004

---
+++ 2604/OnlineShop/Repositories/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace OnlineShop.Repositories
+{
+    public class ProductRepository : BaseRepository
+    {
+        public ProductRepository(MySqlConnection connection) : base(connection)
+        {
+        }
+    }
+}
