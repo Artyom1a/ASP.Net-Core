d7261d3a6f44c58b0407f861d1cf8ace1ae2ff12
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-05-08 18:42:42 +0300

    feature0805n1

---
+++ 2604/OnlineShop/Services/ProductsService.cs
@@ -0,0 +1,28 @@
+﻿using OnlineShop.Models.Repository;
+using OnlineShop.Repositories;
+using OnlineShop.Models.View;
+
+
+namespace OnlineShop.Services
+{
+    public class ProductsService
+    {
+        private readonly ProductRepository p_Product;
+        private readonly BrandRepository p_Brand;
+        private readonly CategoryRepository p_Category;
+
+        public ProductsService(ProductRepository product, BrandRepository brand, CategoryRepository category)
+        {
+            p_Product = product;
+            p_Brand = brand;
+            p_Category = category;
+        }
+        //---------------
+
+        //public List<User> GetAll()
+        //{
+        //    return m_userrep.GetAll();
+        //}
+
+    }
+}
